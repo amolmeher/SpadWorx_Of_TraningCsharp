63dd4c73b5530e358848fd11237f7cecaf12b308
Amol <amolmeher.meher61@gmail.com>
2021-07-11 14:03:30 +0530

    Add use  of base keyword

---
+++ OOP_concept_All/OOP_concept_All/Base_Use_virsual.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class Class1
+{
+	
+        class baseClass
+
+    {
+        public void show()
+        {
+            Console.WriteLine("Base class");
+        }
+    }
+
+    // derived class name 'derived'
+    // 'baseClass' inherit here
+    class derived : baseClass
+    {
+
+        // overriding
+        new public void show()
+        {
+            Console.WriteLine("Derived class");
+        }
+    }
+
+    class GFG
+    {
+
+        // Main Method
+        public static void Main()
+        {
+
+            // 'obj' is the object of
+            // class 'baseClass'
+            baseClass obj = new baseClass();
+
+
+            // invokes the method 'show()'
+            // of class 'baseClass'
+            obj.show();
+
+            obj = new derived();
+
+            // it also invokes the method
+            // 'show()' of class 'baseClass'
+            obj.show();
+
+        }
+    }
+}
